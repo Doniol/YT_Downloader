224dfff622379f260c7378a0eb33620c0e9f73c7
Doniol <43216456+Doniol@users.noreply.github.com>
2022-07-16 23:16:49 +0200

    Added check for existing downloads

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from pytube import Playlist
 import os
+from pathlib import Path
+import re
 
 
 # Where to save the vids
-SAVE_DOWNLOAD = os.path.join(os.path.dirname(__file__), "Downloaded")
+PATH = Path.cwd()
+SAVE_DOWNLOAD = str(PATH.joinpath("Downloaded"))
 
 # Enter playlist
 url = str(input("Enter link to YT-playlist: "))
 convert = str(input("Download only audio instead of entire video? y/n "))
 playlist = Playlist(url)
+existing = os.listdir(SAVE_DOWNLOAD)
 
 # Download playlist
 for video in playlist.videos:
-    try:
-        if convert == 'y':
-            video.streams.get_audio_only().download(SAVE_DOWNLOAD)
-        else:
-            video.streams.get_highest_resolution().download(SAVE_DOWNLOAD)
-    except:
-        print('failed')
+    name = re.sub(r'[<>:\"/\\|?*\'.,$#@!%€&^]', '', video.title) + ".mp4"
+    if name not in existing:
+        try:
+            print(name)
+            if convert == 'y':
+                video.streams.get_audio_only().download(SAVE_DOWNLOAD)
+            else:
+                video.streams.get_highest_resolution().download(SAVE_DOWNLOAD)
+        except:
+            print("failed")
+    else:
+        print("SKIPPEDDDDDDDDDDDDD: " + name)
