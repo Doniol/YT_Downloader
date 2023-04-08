72c18eab5bd37d7276cfe814bc9e8675d7dd5d8e
Doniol <danielvaneijk-bos@live.nl>
2023-04-08 13:51:30 +0200

    Changed how program checks for already downloaded files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pytube import Playlist
 import os
 from pathlib import Path
 import re
+import pickle
 
 
 # Where to save the vids
@@ -9,22 +10,54 @@ PATH = Path.cwd()
 SAVE_DOWNLOAD = str(PATH.joinpath("Downloaded"))
 
 # Enter playlist
+presence = str(input("Do you want to skip previously downloaded files as mentioned in /previous.txt? y/n "))
 url = str(input("Enter link to YT-playlist: "))
 convert = str(input("Download only audio instead of entire video? y/n "))
 playlist = Playlist(url)
 existing = os.listdir(SAVE_DOWNLOAD)
 
+failedVideos = []
+alreadyDownloaded = []
+if presence == 'y' and os.path.isfile("previous.pkl"):
+    with open("previous.pkl", "rb") as file:
+        alreadyDownloaded = pickle.load(file)
+
 # Download playlist
 for video in playlist.videos:
-    name = re.sub(r'[<>:\"/\\|?*\'.,$#@!%€&^]', '', video.title) + ".mp4"
-    if name not in existing:
+    try:
+        title = video.title
+    except Exception as e:
+        failedVideos.append(video.watch_url)
+        print("failed")
+        print(e)
+
+    # For backwards compatibility with versions directly compared names without these characters
+    name = re.sub(r'[<>:\"/\\|?*\'.,$#@!%€&^]', '', title) + ".mp4"
+    # Add the original title to pickle
+    if name in existing:
+        alreadyDownloaded.append(title)
+        with open("previous.pkl", "wb") as file:
+            pickle.dump(alreadyDownloaded, file)
+
+    # Depending on if user wants to redownload files, download the file
+    if presence != 'y' or title not in alreadyDownloaded:
+        alreadyDownloaded.append(title)
+        with open("previous.pkl", "wb") as file:
+            pickle.dump(alreadyDownloaded, file)
+
         try:
-            print(name)
+            print(title)
             if convert == 'y':
                 video.streams.get_audio_only().download(SAVE_DOWNLOAD)
             else:
                 video.streams.get_highest_resolution().download(SAVE_DOWNLOAD)
-        except:
+        except Exception as e:
+            failedVideos.append(video.watch_url)
             print("failed")
+            print(e)
     else:
-        print("SKIPPEDDDDDDDDDDDDD: " + name)
+        print("SKIPPEDDDDDDDDDDDDD: " + title)
+    
+with open("failed.txt", "w") as file:
+    for entry in failedVideos:
+        file.write(entry + "\n")
