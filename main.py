c75fc0fedcffc692a7af49d210f9c91681012fc9
Doniol <danielvaneijk-bos@live.nl>
2025-09-27 15:29:03 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pytube import Playlist, YouTube
+from pytubefix import Playlist, YouTube
 import os
 from pathlib import Path
 import re
@@ -9,15 +9,14 @@ import mutagen
 # Where to save the vids
 PATH = Path.cwd()
 SAVE_DOWNLOAD = str(PATH.joinpath("Downloaded"))
-
-# Enter playlist
-playlists = []
-url = str(input("Enter link to YT-playlist: "))
-while url != "c":
-    playlists.append(Playlist(url))
-    url = str(input("Enter link to next YT-playlist (enter c to cancel): "))
-
 failedVideos = []
+
+playlists = []
+with open(str(PATH.joinpath("playlist-urls.txt")), "r") as file:
+    for line in file:
+        tempUrls = line.strip("\n").split(",")
+        for url in tempUrls:
+            playlists.append(Playlist(url))
 
 for playlist in playlists:
     print(playlist.title)
@@ -27,16 +26,18 @@ for playlist in playlists:
             yt.streams.get_audio_only()
             # Get the name under which the video will be saved
             title = os.path.splitext(yt.streams[0].default_filename)[0]
+            title = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "-", title)
             print(title)
 
             # Download the file
-            if title + ".mp4" not in os.listdir(SAVE_DOWNLOAD):
+            if title + ".m4a" not in os.listdir(SAVE_DOWNLOAD):
                 try:
-                    yt.streams.get_audio_only().download(SAVE_DOWNLOAD)
+                    yt.streams.get_audio_only().download(SAVE_DOWNLOAD, title + ".m4a")
                     print("Downloaded Succesfully")
 
                     # Update file metadata 
-                    with open(str(PATH.joinpath("Downloaded", "{}.mp4".format(title))), "r+b") as file:
+                    print(str(PATH.joinpath("Downloaded", "{}.m4a".format(title))))
+                    with open(str(PATH.joinpath("Downloaded", "{}.m4a".format(title))), "r+b") as file:
                         media_file = mutagen.File(file, easy=True)
                         media_file['artist'] = yt.author
                         media_file['genre'] = playlist.title
@@ -47,9 +48,9 @@ for playlist in playlists:
                     print("Download Failed, Exception Occured: ")
                     print(e)
             
-            elif title + ".mp4" in os.listdir(SAVE_DOWNLOAD):
+            elif title + ".m4a" in os.listdir(SAVE_DOWNLOAD):
                 # Update file metadata 
-                with open(str(PATH.joinpath("Downloaded", "{}.mp4".format(title))), "r+b") as file:
+                with open(str(PATH.joinpath("Downloaded", "{}.m4a".format(title))), "r+b") as file:
                     media_file = mutagen.File(file, easy=True)
                     # If genre not already mentioned, add it
                     genres = media_file.pprint().split("genre=")[1]
